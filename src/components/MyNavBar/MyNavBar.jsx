b11a1a9c3901649e136f78c315814fc69c6d372a
Jason <jrosasrjf@gmail.com>
2021-06-25 13:40:26 -0500

    added in material UI links and hello world test with tutor

---
+++ src/components/MyNavBar/MyNavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyNavBar() {
+    return (
+        <div>
+            <h1> Hello World :D </h1>            
+        </div>
+    )
+}
+
+export default MyNavBar
