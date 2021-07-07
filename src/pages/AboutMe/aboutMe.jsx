69f6c0049e9c425ae29d3e9689cc1b1d5c881d68
Jason <jrosasrjf@gmail.com>
2021-07-06 23:05:40 -0500

    issues with repo again. Pushing updated resume up

--- src/pages/AboutMe/aboutMe.jsx
+++ src/pages/AboutMe/aboutMe.jsx
@@ -8,20 +8,28 @@ const About = () => {
 
             <article>
                 <p>
-                    What do I say about Jason? 
+                    Hello There, 
+
+                    I’m Jason. I am currently finishing up my web development bootcamp at the University of Minnesota - 
+                    Twin Cities school of Continuing education. Science, technology, engineering, and mathematics have 
+                    been interests throughout my life. After gaining hardware experience as an undergraduate physics major, 
+                    I still yearned for more. 
                 </p>
                 <br/>
 
                 <p>
-                    And here is the second paragraph  
+                    After years of working in a professional lab setting, I still wanted to grow my skill set. Enrolling in the 
+                    coding boot camp has rekindled my love for S.T.E.M. that I felt as a child and in my academic years. The course 
+                    is a challenge I enjoy overcoming. The obstacles remind me I have so much to look forward to in learning. 
                 </p>
                 <br/>
 
-
-
-
-
-
+                <p>
+                    Programming is something I have done on the side as part of my passion projects. Configuring microcontrollers to 
+                    perform basic testing and programming a video game are the joys that inspired me to go back. My current web development 
+                    skill set involves HTML, CSS, Javascript, React, Bootstrap, and utilizing APIs. I prefer working on front end development.  
+                </p>
+                <br/>
 
             </article>
 
--- src/pages/Resume/resume.jsx
+++ src/pages/Resume/resume.jsx
@@ -1,14 +1,41 @@
 import React from "react"; 
 import "./resume.css"; 
+import resumeFile from "./WebDevelopmentJasonRosasRamirez.pdf" // the PDF on the same file. 
 // import the pdf file of your resume here
 
 const Resume = () => {
     return (
         <div>
-            <h1>The page for the resume</h1>
+
+            {/* Download Resume button*/}
+            <h3> <a href={resumeFile} download="WebDevelopmentJasonRosasRamirez.pdf"> Download Jason's resume Resume </a> </h3>
 
 
+
+
+            <h1>Education</h1>
+
+            <h2> University of Wisconsin - Eau Claire </h2>
+            <p>Bachelor of Science in Applied Physics</p>
+
+
+            <h1>Programming Experience</h1>
+            <ul>
+                <li> University of Minnesota - Twin Cities Coding Bootcamp </li>
+                <li> NI LabView </li>
+                <li> Javascript </li>
+                <li> C# </li>
+                <li> Python </li>
+            </ul>
+
+
+            <h1> Professional Experience </h1>
+            <ul>
+                <li> Medtronic | Contractor | Sustaining Engineer | June 2019 - Present </li>
+                <li> 3M | Contractor | Electical Engineer l | May 2018 - June 2019  </li>
+            </ul>
         </div>
+
     );
 };
 
